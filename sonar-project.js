44fd1ca3ef62f0dae55a9432d743a65799f0e77d
rohithandique <rohit.handique.rh@gmail.com>
2025-12-15 23:17:40 +0530

    added sonar scan files pt 2

---
+++ sonar-project.js
@@ -0,0 +1,15 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'https://sonarcloud.io',
+  options: {
+    'sonar.organization': 'rohithandique',
+    'sonar.projectKey': 'rohithandique_generic-server-spring-boot',
+    'sonar.sources': 'src',
+    'sonar.tests': 'src',
+    'sonar.inclusions': '**/*.ts,**/*.tsx',
+    'sonar.test.inclusions': 'src/**/*.test.tsx,src/**/*.test.ts',
+    'javascript.lcov.reportPaths': 'coverage/lcov.info',
+    'sonar.token': process.env.SONAR_TOKEN,
+  },
+}, () => process.exit());
