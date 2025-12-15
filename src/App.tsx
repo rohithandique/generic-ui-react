dccce7915a497f16ffa42d3bc38bc2d848b7b3a9
rohithandique <rohit.handique.rh@gmail.com>
2025-12-16 01:15:33 +0530

    removed fragment

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="card">
+    <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
-    </>
+    </div>
   )
 }
 
