640208aa59f428a978a0361927b413017911cac2
rohithandique <rohit.handique.rh@gmail.com>
2025-12-15 23:58:11 +0530

    updated sonar files to cover test cases

--- sonar-project.js
+++ sonar-project.js
@@ -3,13 +3,19 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
   serverUrl: 'https://sonarcloud.io',
   options: {
-    'sonar.organization': 'rohithandique',
-    'sonar.projectKey': 'rohithandique_generic-ui-react',
     'sonar.sources': 'src',
     'sonar.tests': 'src',
-    'sonar.inclusions': '**/*.ts,**/*.tsx',
-    'sonar.test.inclusions': 'src/**/*.test.tsx,src/**/*.test.ts',
-    'javascript.lcov.reportPaths': 'coverage/lcov.info',
-    'sonar.token': process.env.SONAR_TOKEN,
+    'sonar.test.inclusions': 'src/**/*.test.ts,src/**/*.test.tsx',
+    'sonar.exclusions': [
+      'sonar-project.js',
+      'vite.config.ts',
+      'postcss.config.js',
+      'tailwind.config.js',
+      'eslint.config.js',
+      'public/**',
+      'coverage/**',
+      'dist/**'
+    ].join(','),
+    'javascript.lcov.reportPaths': 'coverage/lcov.info'
   },
 }, () => process.exit());
